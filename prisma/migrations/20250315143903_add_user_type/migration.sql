4525b2b7a6e272cd3c2a203d25cb9527d4016d94
Willian Brasil <ntconsult.willian@agi.com.br>
2025-03-15 12:12:21 -0300

    fix: corrige migrations e schema do banco de dados

--- prisma/migrations/20250315143903_add_user_type/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "users" ADD COLUMN     "tipo" TEXT NOT NULL DEFAULT 'paciente';
--- prisma/migrations/20250315150414_fix_agendamento_fields/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "agendamentos" ALTER COLUMN "data" DROP NOT NULL,
-ALTER COLUMN "horario" DROP NOT NULL,
-ALTER COLUMN "profissional" DROP NOT NULL;
